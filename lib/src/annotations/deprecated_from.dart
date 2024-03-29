e4943351a60a68e5ca51ba9c0e21de06f9ec0ed7
Mike <127096658+MichaelDarkMostIo@users.noreply.github.com>
2024-03-29 14:28:06 +0100

    docs: add documentation (#2)

--- lib/src/annotations/deprecated_from.dart
+++ lib/src/annotations/deprecated_from.dart
@@ -1,18 +1,30 @@
+/// Marks a feature as [DeprecatedFrom].
 class DeprecatedFrom {
+  /// Message provided to the user.
   final String message;
+
+  /// Date when this info lint info should become a warning lint.
   final DeprecationDate deprecationDate;
 
+  /// Create a deprecation annotation.
   const DeprecatedFrom(this.message, this.deprecationDate);
 
   @override
   String toString() => "Deprecated feature from $deprecationDate: $message";
 }
 
+/// Marks a specific deprecation deadline for [DeprecatedFrom].
 class DeprecationDate {
+  /// The year.
   final int year;
+
+  /// The month [1..12].
   final int month;
+
+  /// The day [1..31].
   final int dayOfMonth;
 
+  /// Create a deprecation date for [DeprecatedFrom].
   const DeprecationDate(
     this.year,
     this.month,
